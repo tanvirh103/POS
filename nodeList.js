e553380c8f958d5747dcfc643b9821a33d6cba11
tanvir103 <tanvirh103@gmail.com>
2024-02-23 00:46:05 +0600

    .

--- nodeList.js
+++ nodeList.js
@@ -15,40 +15,37 @@ let pool = await readFile('mempool.json', 'utf8');
 let mem = JSON.parse(pool);
 
 let blockInfo = {
-    blockNumber: 123,
-    timestamp: Date.now(),
-    previousBlockHash: "1a2b3c4d5e6f",
-    validator: {
-        publicKey: selectedNode.publicKey,
-        stakingBalance: 1000, 
-        validatorSignature: "3748xutab"+selectedNode.publicKey,
-    },
-    blockHash: "x458jdfjdfdhf",
-    proofOfStake: {
-        stakingReward: 2,
-        stakingDifficulty: 5000,
-    },
+     blockNumber: 123,
+     timestamp: Date.now(),
+     previousBlockHash: "1a2b3c4d5e6f",
+     validator: {
+          publicKey: selectedNode.publicKey,
+          stakingBalance: 1000,
+          validatorSignature: "3748xutab" + selectedNode.publicKey,
+     },
+     blockHash: "x458jdfjdfdhf",
+     proofOfStake: {
+          stakingReward: 2,
+          stakingDifficulty: 5000,
+     },
 };
 
 let blockWithTransactions = {
-    blockInfo: blockInfo,
-    transactions: [],
+     blockInfo: blockInfo,
+     transactions: [],
 };
 
 mem.forEach((transaction, index) => {
-    let transactionInfo = {
-        from: transaction.from,
-        to: transaction.to,
-        amount: transaction.transactionFee,
-        transactionHash: `hash_${index}`,
-    };
-    blockWithTransactions.transactions.push(transactionInfo);
+     let transactionInfo = {
+          from: transaction.from,
+          to: transaction.to,
+          amount: transaction.transactionFee,
+          transactionHash: `hash_${index}`,
+     };
+     blockWithTransactions.transactions.push(transactionInfo);
 });
-
-
-blockWithTransactions.blockInfo.blockHash = calculateBlockHash(blockWithTransactions.blockInfo);
 
 let blockInfoJson = JSON.stringify(blockWithTransactions, null, 2);
 await writeFile('blockInfo.json', blockInfoJson);
 
-console.log("Block information has been written to blockInfo.json");
+console.log("Block information has been written");
