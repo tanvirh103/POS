e88b953ca498fa4e60bd034b6eb305ee415330e0
tanvir103 <tanvirh103@gmail.com>
2024-02-22 23:03:05 +0600

    .

--- nodeList.js
+++ nodeList.js
@@ -15,20 +15,34 @@ console.log("Selected Node's Public Key:",selectedNode.publicKey);
 let pool=await readFile('mempool.json','utf8');
 let mem=JSON.parse(pool);
 
-let transactionInfo = [];
 
-mem.forEach((transaction, index) => {
-    let info = {
-        transactionNumber: index + 1,
-        from: transaction.from,
-        to: transaction.to,
-        amount: transaction.transactionFee,
-    };
-    transactionInfo.push(info);
-});
 
-// Write the structured transaction information into a JSON file
-let transactionInfoJson = JSON.stringify(transactionInfo, null, 2);
-await writeFile('transactionInfo.json', transactionInfoJson);
+let blockInfo = {
+     blockhash: "474574hdfdhfdjfdjfh",  
+     timestamp: Date.now(), 
+     previousblockhash: "udfhfdhfd8347374380dffdfdjddnvvncvj", 
+     validatorPublicKey: selectedNode.publicKey,
+ };
+ 
 
-console.log("Transaction information has been written to transactionInfo.json");
+ let blockWithTransactions = {
+     blockInfo: blockInfo,
+     transactions: [],
+ };
+ 
+
+ mem.forEach((transaction, index) => {
+     let transactionInfo = {
+         transactionNumber: index + 1,
+         from: transaction.from,
+         to: transaction.to,
+         amount: transaction.transactionFee,
+     };
+     blockWithTransactions.transactions.push(transactionInfo);
+ });
+ 
+
+ let blockInfoJson = JSON.stringify(blockWithTransactions, null, 2);
+ await writeFile('blockInfo.json', blockInfoJson);
+ 
+ console.log("Block information has been written");
