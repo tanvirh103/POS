32c76f93ec50beb825623a405b734873c26dbb44
tanvir103 <tanvirh103@gmail.com>
2024-02-22 23:11:07 +0600

    .

--- nodeList.js
+++ nodeList.js
@@ -1,48 +1,46 @@
-import { writeFile,readFile } from 'node:fs/promises';
-let data=await readFile('nodeinfo.json','utf8');
-let parse=JSON.parse(data);
+import { writeFile, readFile } from 'node:fs/promises';
+let data = await readFile('nodeinfo.json', 'utf8');
+let parse = JSON.parse(data);
 
-parse.node.sort((a, b)=> b.stakingCoin - a.stakingCoin);
+parse.node.sort((a, b) => b.stakingCoin - a.stakingCoin);
 
 
 let top3Nodes = parse.node.slice(0, 3);
 
 
-let randomNodes = Math.floor(Math.random()*top3Nodes.length);
-let  selectedNode= top3Nodes[randomNodes];
+let randomNodes = Math.floor(Math.random() * top3Nodes.length);
+let selectedNode = top3Nodes[randomNodes];
 
-console.log("Selected Node's Public Key:",selectedNode.publicKey);
-let pool=await readFile('mempool.json','utf8');
-let mem=JSON.parse(pool);
+console.log("Selected Node's Public Key:", selectedNode.publicKey);
+let pool = await readFile('mempool.json', 'utf8');
+let mem = JSON.parse(pool);
 
 
 
 let blockInfo = {
-     blockhash: "474574hdfdhfdjfdjfh",  
-     timestamp: Date.now(), 
-     previousblockhash: "udfhfdhfd8347374380dffdfdjddnvvncvj", 
+     blockhash: "474574hdfdhfdjfdjfh",
+     timestamp: Date.now(),
+     previousblockhash: "udfhfdhfd8347374380dffdfdjddnvvncvj",
      validatorPublicKey: selectedNode.publicKey,
- };
- 
-
- let blockWithTransactions = {
+};
+let blockWithTransactions = {
      blockInfo: blockInfo,
      transactions: [],
- };
- 
+};
 
- mem.forEach((transaction, index) => {
+
+mem.forEach((transaction, index) => {
      let transactionInfo = {
-         transactionNumber: index + 1,
-         from: transaction.from,
-         to: transaction.to,
-         amount: transaction.transactionFee,
+          transactionNumber: index + 1,
+          from: transaction.from,
+          to: transaction.to,
+          amount: transaction.transactionFee,
      };
      blockWithTransactions.transactions.push(transactionInfo);
- });
- 
+});
 
- let blockInfoJson = JSON.stringify(blockWithTransactions, null, 2);
- await writeFile('blockInfo.json', blockInfoJson);
- 
- console.log("Block information has been written");
+
+let blockInfoJson = JSON.stringify(blockWithTransactions, null, 2);
+await writeFile('blockInfo.json', blockInfoJson);
+
+console.log("Block information has been written");
